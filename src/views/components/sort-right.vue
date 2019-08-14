96780d767b1278bd5dd2b5ceec70fb114802a87a
weiaijie <weiaijie@Outlook.com>
2019-08-14 19:13:29 +0800

    Update sort-right.vue

---
+++ src/views/components/sort-right.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="sort_rigth">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sortRight',
+  data() {
+    return{
+
+    }
+  },
+  created() {
+    
+  }
+}
+</script>
