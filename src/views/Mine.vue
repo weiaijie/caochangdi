59799cc5fe9cb0ff318300e112e464682266e241
weiaijie <weiaijie@Outlook.com>
2019-08-19 23:55:45 +0800

    Update Mine.vue

--- src/views/Mine.vue
+++ src/views/Mine.vue
@@ -1,19 +1,23 @@
 <template>
-    <div class="mine">
-
+  <div class="mine">
+    <div class="mod_section">
+      <div class="mod_user_head"></div>
     </div>
+    <div class="mod_section"></div>
+    <div class="mod_section"></div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'mine',
-    components: {
+  name: 'mine',
+  components: {
 
-    },
-    data() {
-        return{
+  },
+  data() {
+    return{
 
-        }
     }
+  }
 }
 </script>
